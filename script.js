e1e8487c2684a14f19cf8e9c424015a912d7aec9
Uzairkhan-ux <uzairkhan.me9@gmail.com>
2024-10-22 19:15:40 +0500

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ coin.addEventListener('click', () => {
   score++;
   scoreElement.textContent = score;
 });
+function moveCoin() {
+  const randomX = Math.floor(Math.random() * window.innerWidth);
+  const randomY = Math.floor(Math.random() * window.innerHeight);
+  coin.style.left = randomX + 'px';
+  coin.style.top = randomY + 'px';
+}
+
+setInterval(moveCoin, 1000); // Move the coin every 1000 milliseconds (1 second)
